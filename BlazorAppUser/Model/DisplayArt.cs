877a7f63ac0ee4b5deb5a71c92394b2cffde8ad5
thistype <jkuria378@gmail.com>
2024-01-18 14:33:17 +0300

    Add project files.

---
+++ BlazorAppUser/Model/DisplayArt.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorAppUser.Model
+{
+    public class DisplayArt
+    {
+
+
+        public Guid ArtId { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }    
+        public string category { get; set; }
+
+
+        public int StartingPrice { get; set; }
+
+            
+        public string Img{  get; set; }
+    }
+}
